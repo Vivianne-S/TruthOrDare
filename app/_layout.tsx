65b5c441f6b5b63005c178726ff712d1a9a56e91
angie <3ggapp24.ekmaan@folkuniversitetet.nu>
2026-02-18 15:42:47 +0100

    Revert "Created Add players screen and added some avatars"
    This reverts commit 084bde0deb2a0a591d51419d35a1de5ee906ccc7.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="how-to-play" options={{ headerShown: false }} />
-        <Stack.Screen name="add-players" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
       </Stack>
