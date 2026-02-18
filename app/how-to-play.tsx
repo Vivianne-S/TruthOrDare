edfdf33f78bd9c34def1740e6272f811b7d06e44
Angelica Ekman <3ggapp24.ekmaan@folkuniversitetet.nu>
2026-02-18 14:54:55 +0100

    Update background image source in how-to-play component

--- app/how-to-play.tsx
+++ app/how-to-play.tsx
@@ -48,7 +48,7 @@ export default function HowToPlayScreen() {
 
   return (
     <ImageBackground
-      source={require('@/assets/images/instruction_circle_background.jpeg')}
+      source={require('@/assets/images/instruction_pic.jpeg')}
       resizeMode="cover"
       style={styles.background}
     >
